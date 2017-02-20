ee95f0fd39d11ddada12ea48f40bf73727775ed0
Arnold Wong <awong@Arnolds-MBP.vc.shawcable.net>
2017-02-20 15:45:02 -0800

    Celeste project initial commit

---
+++ src/main/java/com/celeste/CelesteApplication.java
@@ -0,0 +1,12 @@
+package com.celeste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CelesteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CelesteApplication.class, args);
+	}
+}
